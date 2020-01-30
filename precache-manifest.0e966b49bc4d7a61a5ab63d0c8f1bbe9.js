8a29db1df37d6372e60e0f2352dfdd7f5e6250cc
sbouaziz-lvmh <soukaina.bouaziz@gmail.com>
2020-01-30 16:13:41 +0000

    Updates

---
+++ precache-manifest.0e966b49bc4d7a61a5ab63d0c8f1bbe9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c981fa6330de1ec9dc93470bda721db4",
+    "url": "/souki1990/shopping-list/index.html"
+  },
+  {
+    "revision": "289b568555a95c122354",
+    "url": "/souki1990/shopping-list/static/css/main.3bdc6ada.chunk.css"
+  },
+  {
+    "revision": "68c86d0a47e2a79ef695",
+    "url": "/souki1990/shopping-list/static/js/2.6d8c21ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/souki1990/shopping-list/static/js/2.6d8c21ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "289b568555a95c122354",
+    "url": "/souki1990/shopping-list/static/js/main.98fea5e8.chunk.js"
+  },
+  {
+    "revision": "040ae812a7697650edad",
+    "url": "/souki1990/shopping-list/static/js/runtime-main.837b6e3c.js"
+  },
+  {
+    "revision": "ff4cb077becddf77245edf684970c560",
+    "url": "/souki1990/shopping-list/static/media/control_point-24px.ff4cb077.svg"
+  },
+  {
+    "revision": "fc8d7b3b1b300b6b9d1b0037ad57a4b3",
+    "url": "/souki1990/shopping-list/static/media/delete_forever-24px.fc8d7b3b.svg"
+  },
+  {
+    "revision": "d98a6a66fa22ecfe00fd041d16ed1607",
+    "url": "/souki1990/shopping-list/static/media/logo.d98a6a66.svg"
+  }
+]);
